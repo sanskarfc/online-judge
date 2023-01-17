5fabd3c917adb01eeec14219ddfedab561e549a6
Sanskar Sharma <sanskarsharma@Sanskars-MacBook-Air.local>
2023-01-17 09:48:41 +0530

    added the react-code-editor with syntax highlighting (testing)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css'; //Example style, you can use another
 
 function App() {
+  const [code, setCode] = React.useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  );
   return (
-    <div className="App">        
-
+    <div className='editor-view'>
+        <h1>Code Editor using React Code Editor</h1>
+        <Editor
+          value={code}
+          onValueChange={code => setCode(code)}
+          highlight={code => highlight(code, languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
+        />
     </div>
   );
 }
 
 export default App;
+
